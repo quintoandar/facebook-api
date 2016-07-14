0b0edd81ed4812bd1e207c043aeb48c7a1fb4fbd
akiroso <akira@quintoandar.com.br>
2016-07-14 19:11:48 -0300

    fixing request parameters

--- src/main/java/br/com/quintoandar/facebook/api/lead/LeadAPI.java
+++ src/main/java/br/com/quintoandar/facebook/api/lead/LeadAPI.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import br.com.quintoandar.facebook.api.filter.Filter;
@@ -17,16 +18,16 @@ public interface LeadAPI {
 	@GET
 	@Path("/{formId}/leads")
 	@Produces(MediaType.APPLICATION_JSON)
-	public LeadList listFormLeads(@FormParam("access_token") String auth, @PathParam("formId") String formId, @FormParam("filtering") List<Filter> filter);
+	public LeadList listFormLeads(@QueryParam("access_token") String auth, @PathParam("formId") String formId, @FormParam("filtering") List<Filter> filter);
 	
 	@GET
 	@Path("/{adId}/leads")
 	@Produces(MediaType.APPLICATION_JSON)
-	public LeadList listAdLeads(@FormParam("access_token") String auth, @PathParam("adId") String formId, @FormParam("filtering") List<Filter> filter);
+	public LeadList listAdLeads(@QueryParam("access_token") String auth, @PathParam("adId") String formId, @FormParam("filtering") List<Filter> filter);
 	
 	@GET
 	@Path("/{leadId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Lead getLead(@FormParam("access_token") String auth, @PathParam("leadId") String leadId);
+	public Lead getLead(@QueryParam("access_token") String auth, @PathParam("leadId") String leadId);
 	
 }
