00b74a69ded4c6a0660ed7a1c8dfecff9df95521
akiroso <akira@quintoandar.com.br>
2016-07-18 14:56:17 -0300

    fix method annotation

--- src/main/java/br/com/quintoandar/facebook/api/form/FormAPI.java
+++ src/main/java/br/com/quintoandar/facebook/api/form/FormAPI.java
@@ -1,13 +1,18 @@
 package br.com.quintoandar.facebook.api.form;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @Path("/v2.7")
 public interface FormAPI {
 
+	@GET
 	@Path("/{pageId}/leadgen_forms")
+	@Produces(MediaType.APPLICATION_JSON)
 	public FormList getFormList(@QueryParam("access_token") String accessToken, @PathParam("pageId") String pageId);
 	
 }
