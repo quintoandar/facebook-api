31bb9cd6041593fa77b37bb8ba3aa6badc07594a
Italo Garcia <italo.garcia@quintoandar.com.br>
2018-09-04 19:19:20 -0300

    Implementing API to use Facebook Audience

--- src/main/java/br/com/quintoandar/facebook/api/FacebookAPI.java
+++ src/main/java/br/com/quintoandar/facebook/api/FacebookAPI.java
@@ -1,5 +1,9 @@
 package br.com.quintoandar.facebook.api;
 
+import br.com.quintoandar.facebook.api.audience.Audience;
+import br.com.quintoandar.facebook.api.audience.AudienceAPI;
+import br.com.quintoandar.facebook.api.audience.BatchUserUpdate;
+import br.com.quintoandar.facebook.api.common.Success;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,14 +24,19 @@ public class FacebookAPI {
 	private String accessToken;
 	
 	private String facebookPageId;
+
+	private String adAccountId;
 	
 	private LeadAPI leadApi;
 	
 	private FormAPI formApi;
+
+	private AudienceAPI audienceAPI;
 	
-	public FacebookAPI(String baseUrl, String accessToken, String pageId) {
+	public FacebookAPI(String baseUrl, String accessToken, String pageId, String adAccountId) {
 		this.accessToken = accessToken;
 		this.facebookPageId = pageId;
+		this.adAccountId = adAccountId;
 		
 		ResteasyClient client = new ResteasyClientBuilder().build();
 		
@@ -35,6 +44,7 @@ public class FacebookAPI {
 
 		leadApi = target.proxy(LeadAPI.class);
 		formApi = target.proxy(FormAPI.class);
+		audienceAPI = target.proxy(AudienceAPI.class);
 	}
 	
 	public Lead getLead(String leadId) {
@@ -56,6 +66,29 @@ public class FacebookAPI {
 	
 	public FormList getPageForms() {
 		return formApi.getFormList(accessToken, this.facebookPageId);
+	}
+
+	public Audience createAudience(
+			String adAccountId,
+			String customerFileSource,
+			String auth,
+			String name,
+			String description,
+			String subtype
+	) {
+		return audienceAPI.createAudience(adAccountId, customerFileSource, auth, name, description, subtype);
+	}
+
+	public Success deleteAudience(String customAudienceId) {
+		return audienceAPI.deleteAudience(customAudienceId);
+	}
+
+	public BatchUserUpdate insertUserInAudience(String customAudienceId, String payload) {
+		return audienceAPI.insertUserInAudience(customAudienceId, payload);
+	}
+
+	public BatchUserUpdate removeUserFromAudience(String customAudienceId, String payload) {
+		return audienceAPI.removeUserFromAudience(customAudienceId, payload);
 	}
 		
 }
---
+++ src/main/java/br/com/quintoandar/facebook/api/audience/AudienceAPI.java
@@ -0,0 +1,49 @@
+package br.com.quintoandar.facebook.api.audience;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.quintoandar.facebook.api.common.Success;
+
+@Path("/v2.11")
+public interface AudienceAPI {
+
+  @POST
+  @Path("/act_{adAccountId}/customaudiences")
+  @Produces(MediaType.APPLICATION_JSON)
+  Audience createAudience(
+      @PathParam("adAccountId") String adAccountId,
+      @FormParam("customer_file_source") String customerFileSource,
+      @FormParam("access_token") String auth,
+      @FormParam("name") String name,
+      @FormParam("description") String description,
+      @FormParam("subtype") String subtype
+  );
+
+  @DELETE
+  @Path("/{customAudienceId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  Success deleteAudience(@PathParam("customAudienceId") String customAudienceId);
+
+  @POST
+  @Path("/{customAudienceId}/users")
+  @Produces(MediaType.APPLICATION_JSON)
+  BatchUserUpdate insertUserInAudience(
+      @PathParam("customAudienceId") String customAudienceId,
+      String payload
+  );
+
+  @DELETE
+  @Path("/{customAudienceId}/users")
+  @Produces(MediaType.APPLICATION_JSON)
+  BatchUserUpdate removeUserFromAudience(
+      @PathParam("customAudienceId") String customAudienceId,
+      String payload
+  );
+
+}
